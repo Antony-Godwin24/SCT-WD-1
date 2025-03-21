42ee6e5762a5322661a9e0ad06e840895083da28
ANTONY GODWIN S <antonygodwin08@gmail.com>
2025-03-21 11:26:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Change navbar background when scrolling
+window.addEventListener("scroll", function() {
+    let navbar = document.getElementById("nav");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
